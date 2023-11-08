18f1541888e9d7fa329eef3958bda8910e843b1e
Lukas <bartulu2@fel.cvut.cz>
2023-11-09 00:44:48 +0100

    Moved files with image data into data folder.

--- utils.py
+++ utils.py
@@ -2,18 +2,20 @@ __author__ = 'Lukáš Bartůněk'
 
 import os
 import natsort
-import  pathlib
+import pathlib
 
-def prepare_paths(pth,abs_p):
+
+def prepare_paths(pth, abs_p):
     if abs_p:
         abs_pth = pth
     else:
         abs_pth = os.getcwd() + pth  # absolute file of image directory
     folder_name = abs_pth.split("/")[-1]  # name of most nested folder for better naming of resulting files
-    sim_path = "image_similarities_" + folder_name + ".json"  # file to save result of precalculating similarities
-    q_path = "image_quality_" + folder_name + ".json"  # file to save result of quality evaluation
-    content_path = "image_content_" + folder_name + ".json" # file to save result of image content evaluation
+    sim_path = "data/image_similarity_" + folder_name + ".json"  # file to save result of precalculating similarities
+    q_path = "data/image_quality_" + folder_name + ".json"  # file to save result of quality evaluation
+    content_path = "data/image_content_" + folder_name + ".json"  # file to save result of image content evaluation
     return abs_pth, sim_path, q_path, content_path
+
 
 def prepare_img_list(pth):
     # assuming the files are numbered based on the sequence they have been taken in
@@ -22,6 +24,11 @@ def prepare_img_list(pth):
     list(global_path.rglob("*.jpg"))
     for p in list(global_path.rglob("*.jpg")):
         img_list.append(str(p))
-    img_num = len(img_list)
     img_list = natsort.natsorted(img_list)
-    return img_list,img_num
+    return img_list, len(img_list)
+
+
+def remove_folder_name(lst, f):
+    for i, l in enumerate(lst):
+        lst[i] = l.replace(str(f + "/"), "")
+    return lst
